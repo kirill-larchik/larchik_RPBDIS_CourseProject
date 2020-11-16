2d0b2c8f596e95ab23a6e458560cc7e97f0554f8
Kirill Larchik <lar4ik.kirill@outlook.com>
2020-11-16 10:47:23 +0300

    Add models.

---
+++ CourseProject/WebApplication/Models/Appeal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Appeal
+    {
+        public int AppealId { get; set; }
+
+        [Required]
+        [Display(Name = "Full name")]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Full name length must be between 8 and 64 symbols.")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "Organization")]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Organization length must be between 8 and 64 symbols.")]
+        public string Organization { get; set; }
+
+        [Display(Name = "Show")]
+        public int ShodId { get; set; }
+
+        [Required]
+        [Display(Name = "Goal request")]
+        [StringLength(128, MinimumLength = 8, ErrorMessage = "Goal request length must be between 8 and 64 symbols.")]
+        public string GoalRequest { get; set; }
+
+        public Show Show { get; set; }
+    }
+}
---
+++ CourseProject/WebApplication/Models/Staff.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class Staff
+    {
+        public Staff()
+        {
+            Timetables = new HashSet<Timetable>();
+        }
+
+        public int StaffId { get; set; }
+
+        [Required]
+        [Display(Name = "Full name")]
+        [StringLength(64, MinimumLength = 8, ErrorMessage = "Full name length must be between 8 and 64 symbols.")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Position")]
+        public int PositionId { get; set; }
+
+        public Position Position { get; set; }
+
+        public ICollection<Timetable> Timetables { get; set; }
+    }
+}
